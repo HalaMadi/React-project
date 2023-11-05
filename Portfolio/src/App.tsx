4f37dbd9741703fe2d2ca100cff8323cdd481c84
Hala Madi <halamadi75@gmail.com>
2023-11-05 12:01:59 +0300

    Create component

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+// import Menu from "./components/menu/menu.tsx";
+// import Projects from './components/projects/projects.tsx';
+import Header from './components/header/header.tsx';
 function App() {
   return (
-    <div>App</div>
+    <Header/>
+   
   )
 }
 
---
+++ Portfolio/src/components/menu/menu.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./menu.css";
+function menu() {
+  return (
+    <div>menu</div>
+  )
+}
+
+export default menu
